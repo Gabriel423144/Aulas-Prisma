e7b13eff47823bc0390c1d52f754a07a05336262
Gabriel423144 <gabriel.ferreira.pc4@gmail.com>
2025-09-11 15:25:09 -0300

    Update atividade-8.js

--- Atividades Javascript/atividade-8.js
+++ Atividades Javascript/atividade-8.js
@@ -1,9 +1,9 @@
-let nota = 3
+let nota = 4
 
 if (nota >= 6) {
     console.log("Parabens voce foi aprovado")
-}else if(nota >= 5) {
+}else if(nota <= 5) {
     console.log("voce esta de recuperação")
-} else if (nota >= 3) {
+} else if (nota <= 3) {
     console.log("voce esta reprovado")
 }
