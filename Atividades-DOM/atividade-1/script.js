fdd53096a42cca217da5168965d5501388d23742
Gabriel423144 <gabriel.ferreira.pc4@gmail.com>
2025-09-23 15:24:07 -0300

    Dom js

--- Atividades-DOM/atividade-1/script.js
+++ Atividades-DOM/atividade-1/script.js
@@ -2,15 +2,27 @@ let aumentar = document.getElementById("BotaoIncrementar");
 let contador = 0;
 let diminuir = document.getElementById("BotaoDecrementar");
 const texto = document.getElementById("titulo");
-const body =document.getElementById("corpo")
-body.style.backgroundColor = "White"
-
-aumentar.addEventListener ("click", () => {
-    contador++
-texto.innerText = "contador" + contador
-})
-;
-diminuir.addEventListener ("click", () => {
-    contador--;
-  texto.innerText = "contador" + contador
+const body = document.getElementById("corpo");
+let botao = document.getElementById("Add");
+let lista = document.getElementById("lista");
+let botaoAzul = document.getElementById("azul");
+let botaoVermelho = document.getElementById("vermelho");
+let botaoVerde = document.getElementById("verde");
+let Seletor = document.getElementById("seletor");
+body.style.backgroundColor = "gray";
+botao.addEventListener("click", () => {
+  let item = document.createElement("li");
+  item.innerText = "novo item";
+  lista.appendChild(item);
+});
+aumentar.addEventListener("click", () => {
+  contador++;
+  texto.innerText = "contador" + contador;
+});
+diminuir.addEventListener("click", () => {
+  contador--;
+  texto.innerText = "contador" + contador;
+});
+Seletor.addEventListener("input", (color) => {
+  body.style.backgroundColor = Seletor.value;
 });
