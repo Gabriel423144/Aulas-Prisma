bcd31184be55db356e2f075fc284f36906edec76
Gabriel423144 <gabriel.ferreira.pc4@gmail.com>
2025-09-25 14:52:12 -0300

    alterações

--- Atividades-DOM/atividade-1/script.js
+++ Atividades-DOM/atividade-1/script.js
@@ -10,19 +10,31 @@ let botaoVermelho = document.getElementById("vermelho");
 let botaoVerde = document.getElementById("verde");
 let Seletor = document.getElementById("seletor");
 body.style.backgroundColor = "gray";
+const btnRemover = document.getElementById("BotaoRemover");
+
+btnRemover.addEventListener("click", () =>{
+  if (lista.lastChild) {
+    lista.removeChild(lista.lastChild);
+  }
+})
+
 botao.addEventListener("click", () => {
   let item = document.createElement("li");
   item.innerText = "novo item";
   lista.appendChild(item);
 });
+
 aumentar.addEventListener("click", () => {
   contador++;
   texto.innerText = "contador" + contador;
 });
+
 diminuir.addEventListener("click", () => {
   contador--;
   texto.innerText = "contador" + contador;
 });
+
 Seletor.addEventListener("input", (color) => {
   body.style.backgroundColor = Seletor.value;
 });
+
